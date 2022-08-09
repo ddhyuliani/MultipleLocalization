e2946087de849bc44f3d30c8722483fa7780e358
Dian DInihari | Tia <55896324+ddhyuliani@users.noreply.github.com>
2022-08-10 05:22:04 +0700

    update StarterProject

--- MultipleLocalization/MultipleLocalization/ViewController.swift
+++ MultipleLocalization/MultipleLocalization/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var randomButton: UIButton!
+    
+    let funFactData = ["Bats are the only mammal that can actually fly. 🦇",
+                       "It’s impossible to hum while holding your nose, try it! 👃",
+                       "It's illegal to own just one guinea pig in Switzerland. \nIt's considered animal abuse because they're social beings and get lonely. 🐹",
+                       "The Japanese word 'Kuchi zamishi' is the act of eating when you're not hungry because your mouth is lonely. 🇯🇵",
+                       "Octopuses have three hearts. \nOne heart circulates blood around the body, while the other two pump it past the gills, to pick up oxygen. 🐙"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        randomButton.layer.cornerRadius = 8
+        funFactLabel.text = funFactData[0]
     }
 
-
+    @IBAction func randomize(_ sender: Any) {
+        let randomFacts = funFactData.shuffled()
+        let funFact = randomFacts[0]
+        
+        funFactLabel.text = funFact
+    }
+    
 }
 
